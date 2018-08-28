ccabdf654a785f9fe9cf0a7aab0704dcbfcca43d
Jim M <Jim.R.Maloney@gmail.com>
2018-08-28 00:11:09 -0400

    Update learnjs

--- client/src/components/Learn.js
+++ client/src/components/Learn.js
@@ -6,9 +6,9 @@ class Learn extends Component  {
     render(){
         return (
         <div>
-            <h1>
-                Learn Page
-            </h1>
+            <iframe src="https://jobscan.co/iframe?btncolor=1f57b5&utm_source=rutgersedubootcamp&utm_medium=iframe"width="800" height="650">
+               <p>Your browser does not support iframes.</p>
+               </iframe>
         </div>
         );
     }
